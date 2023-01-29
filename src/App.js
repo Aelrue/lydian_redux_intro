530aac37cf648b567e8003cfe805d4ae3724be45
Aelrue <101987454+Aelrue@users.noreply.github.com>
2023-01-29 12:08:30 -0600

    added notes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import './App.css';
+import "./App.css";
 
 // useSelector is a hook provided by the react-redux library
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import ElementList from "./ElementList";
 
-function App () {
-    // useSelector accepts a function that tells it what part of the store you want.
-    // here we return the whole store
-    const reduxStore = useSelector(store => store)
-    const count = useSelector(store => store.count)
+function App() {
+  // useSelector accepts a function that tells it what part of the store you want.
+  // here we return the whole store
+  const reduxStore = useSelector((store) => store);
+  const count = useSelector((store) => store.count);
+  const elementList = useSelector((store) => store.elementList);
 
+  const dispatch = useDispatch();
 
-    return (
-      <div>
-        {/* Render the entire reduxStore to our DOM, as a JS object (JSON) */}
-        <pre>{JSON.stringify(reduxStore)}</pre>
-        <p>Count is: {count}</p>
-
-      </div>
-    );
-
+  const [newElement, setNewElement] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: "ADD_ELEMENT",
+      payload: newElement,
+      // almost like an axios request, but instead of making request to server,
+      // we're making one to the redux store
+    });
+    setNewElement("");
+  };
+  return (
+    <div>
+      {/* Render the entire reduxStore to our DOM, as a JS object (JSON) */}
+      <pre>{JSON.stringify(reduxStore)}</pre>
+      <p>Count is: {count}</p>
+      <button onClick={() => dispatch({ type: "INCREASE" })}>
+        Increase Count
+      </button>
+      <button onClick={() => dispatch({ type: "DECREASE" })}>
+        Decrease Count
+      </button>
+      {/* <p>Elements are: {JSON.stringify(elementList)}</p> */}
+      <ElementList />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Element Name Goes Here"
+          value={newElement}
+          onChange={(event) => setNewElement(event.target.value)}
+        />
+        <button type="submit">Add Element</button>
+      </form>
+      {/* <button
+        onClick={() => dispatch({ type: "ADD_ELEMENT", payload: "radium" })}
+      >
+        Add Element
+      </button> */}
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import { act } from "react-dom/test-utils";
 
 // reducer!
 const count = (state = 0, action) => {
-    console.log(`Hey!!! I'm a reducer y'all!!!`);
-
-    return state;
+  console.log(`Hey!!! I'm a reducer y'all!!!`);
+  console.log("action", action);
+  if (action.type === "INCREASE") {
+    return state + 1;
+    // OR state ++
+    // state +=1
+    // state = state +1
+  } else if (action.type === "DECREASE") {
+    return state - 1;
+  }
+  // no matter which route you hit, you should then get kicked out of the function
+  // but if using multiple conditionals, you could possibly hit both routes
+  // so using if else as a safeguard
+  return state;
 };
 
-const elementList = (state = [], action) => {
+const elementList = (state = ["oxygen", "hydrogen"], action) => {
+  console.log("action inside elementList", action.payload);
+  if (action.type === "ADD_ELEMENT") {
+    // state being the array that's being pushed to
+    // .push mutates the original state, but react and redux need us to create new instances
+    // of each values for it to trigger and rerender the value of elementList. That's where spread operators come in ...
+    // state.push(action.payload);
+    // return state;
+    let newArray = [...state, action.payload];
+    return newArray;
+  }
   return state;
 };
 
 // store!
 const storeInstance = createStore(
-  combineReducers(
-      {
-          count,
-          elementList,
-      }
-  )
+  combineReducers({
+    count,
+    elementList,
+  }),
+  applyMiddleware(logger)
+  //
 );
 
 // Provider lets redux and react talk to one another
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={storeInstance}>
-        <App />
+      <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
